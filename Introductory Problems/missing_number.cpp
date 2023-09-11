b49ce70de7b186fafcf771514022493646318e20
NirSeg <99889837+NirSeg@users.noreply.github.com>
2023-09-11 11:54:38 +0300

    added the two first problems ;)

---
+++ Introductory Problems/missing_number.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+#define int int64_t
+
+#define for_i(n, i) for (int i=0; i<n; i++)
+#define all(x) x.begin(),x.end()
+using vi = vector<int>;
+
+void fillArray(vi &arr, int len) {
+    int a;
+    for_i(len, i) {
+        cin >> a;
+        arr.push_back(a);
+    }
+}
+
+// supposed sum: num * (num + 1) / 2
+// sum is the sum of the array
+// if num is even occurs: sum % (num + 1) = num + 1 - missing_num
+// and if num is odd then: sum % num = num - missing_num
+
+void missing_number(vi arr, int num) {
+    int sum = 0;
+    int temp;
+
+    if (num % 2 == 0)
+        temp = num + 1;
+    else
+        temp = num;
+
+    for_i(num - 1, i)
+        sum = (sum + arr[i]) % temp;
+    int missing_num = temp - sum;
+    cout << missing_num;
+}
+
+signed main() {
+    int num;
+    cin >> num;
+    int len = num - 1;
+    vi arr;
+    fillArray(arr, len);
+    missing_number(arr, num);
+}
--- Introductory Problems/weird_algorithm.cpp
+++ Introductory Problems/weird_algorithm.cpp
@@ -1,4 +1,21 @@
-//
-// Created by User on 9/9/2023.
-//
-// bla vla
+#include <bits/stdc++.h>
+
+using namespace std;
+#define int int64_t
+
+// get a positive integer
+void weird_algorithm(int num) {
+    cout << num << " ";
+    if (num == 1)
+        return;
+    else if (num % 2 == 0)
+        weird_algorithm(num / 2);
+    else
+        weird_algorithm(3 * num + 1);
+}
+
+signed main() {
+    int num;
+    cin >> num;
+    weird_algorithm(num);
+}
