4ef028b94fdc8f61a1942d1fc34a81a9cb9d0a71
NirSeg <99889837+NirSeg@users.noreply.github.com>
2023-11-08 15:58:27 +0200

    [Concert Tickets](https://cses.fi/problemset/task/1091/)

--- CSES Problem Set/Sorting and Searching/concertTickets.cpp
+++ CSES Problem Set/Sorting and Searching/concertTickets.cpp
@@ -2,25 +2,27 @@
 
 using namespace std;
 #define int int64_t
-#define rep(i, a, b) for(int i = a; i < b; ++i)
+#define all(x) begin(x), end(x)
+using vi = vector<int>;
 
-void concertTickets(multiset<int> &hs, int m) {
-    int maxPrice, ticket;
-    rep(i, 0, m){
-        scanf("%lld", &maxPrice);
-        auto ticketIter = hs.lower_bound(maxPrice);
-        if (ticketIter != hs.begin() || *ticketIter == maxPrice){
-            ticket = *ticketIter == maxPrice ? *ticketIter : *(--ticketIter);
-            hs.erase(ticketIter);
-        } else { ticket = -1; }
-        printf("%lld\n", ticket);
+void concertTickets(vi &hs, int m){
+    int t, i, res;
+    vi links(hs.size() + 1); iota(all(links), 0);
+    sort(all(hs));
+    while(m--){
+        scanf("%lld", &t);
+        i = upper_bound(all(hs), t) - hs.begin();
+        while (i != links[i]) i = links[i] = links[links[i]];
+        if(i==0) res = -1;
+        else { res = hs[i - 1]; links[i]--; }
+        printf("%lld\n", res);
     }
 }
 
-signed main() {
-    int n, m, a;
+signed main(){
+    int n, m;
     cin >> n >> m;
-    multiset<int> hs;
-    rep(i, 0, n) { scanf("%lld", &a); hs.insert(a); }
+    vi hs(n);
+    for (auto &h : hs) scanf("%lld", &h);
     concertTickets(hs, m);
 }
