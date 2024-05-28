40c6d3de644fc56b2b3b61c3ef87a013cff9589a
NirSeg <nirsegal2004@gmail.com>
2024-05-28 23:13:38 +0300

    [Maximum Subarray Sum](https://cses.fi/problemset/task/1643/)

---
+++ CSES Problem Set/Sorting and Searching/maximumSubarraySum.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+#define int int64_t
+using vi = vector<int>;
+
+#define rep(i, a, b) for(int i = a; i < b; ++i)
+
+int maximumSubarraySum(int n){
+    int a;
+    cin >> a;
+    int sum = a, maxSum = a;
+    rep(i, 1, n){
+        cin >> a;
+        sum = sum > 0 ? sum + a : a;
+        if (sum > maxSum)
+            maxSum = sum;
+    }
+    return maxSum;
+}
+
+signed main(){
+    int n;
+    cin >> n;
+    cout << maximumSubarraySum(n);
+}
