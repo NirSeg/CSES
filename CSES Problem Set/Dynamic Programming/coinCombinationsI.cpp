3cfab5847a173e4d4af3ff1f5954033888e99a0b
NirSeg <nirsegal2004@gmail.com>
2024-07-06 10:12:21 +0300

    [Coin Combinations I](https://cses.fi/problemset/task/1635)

---
+++ CSES Problem Set/Dynamic Programming/coinCombinationsI.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+typedef long long ll;
+
+#define rep(i, a, b) for(int i = a; i < b; i++)
+
+ll coinCombinationI(ll x, const vector<ll> &coins, ll n) {
+    vector<ll> dp(x + 1, 0);
+    ll mod = 1e9 + 7;
+    dp[0] = 1;
+    rep(i, 1, x + 1) {
+        for (ll j = 0; j < n && coins[j] <= i; j++)
+            (dp[i] += dp[i - coins[j]]) %= mod;
+    }
+    return dp[x];
+}
+
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    ll n, x;
+    cin >> n >> x;
+    vector<ll> coins(n);
+    rep(i, 0, n) cin >> coins[i];
+    sort(coins.begin(), coins.end());
+    cout << coinCombinationI(x, coins, n);
+}
