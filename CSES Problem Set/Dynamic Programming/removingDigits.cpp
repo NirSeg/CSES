a32661193774f9095a22d2019c180cac29f65f79
NirSeg <nirsegal2004@gmail.com>
2024-07-09 22:47:41 +0300

    [Removing Digits](https://cses.fi/problemset/task/1638)

---
+++ CSES Problem Set/Dynamic Programming/removingDigits.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+typedef long long ll;
+#define rep(i, a, b) for (ll i = a; i < b; i++)
+
+ll removingDigits(ll n) {
+    vector<ll> dp(n + 1, 1e6);
+    dp[0] = 0;
+    string num;
+    rep(i, 1, n + 1){
+        num = to_string(i);
+        for(char ch : num)
+            dp[i] = min(dp[i], dp[i - (ch - '0')] + 1);
+    }
+    return dp[n];
+}
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    ll n;
+    cin >> n;
+    cout << removingDigits(n);
+}
