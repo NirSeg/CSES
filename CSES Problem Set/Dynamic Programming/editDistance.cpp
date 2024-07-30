1dcd9f260500c7ac907235a71539576076b270e2
NirSeg <nirsegal2004@gmail.com>
2024-07-30 16:58:30 +0300

    [Edit  Distance](https://cses.fi/problemset/task/1639)

---
+++ CSES Problem Set/Dynamic Programming/editDistance.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+#define rep(i, a, b) for(ll i = a; i < b; i++)
+
+ll editDistance(string str1, string str2){
+    ll last, curr, n = ll(str1.size()), m = ll(str2.size());
+    vector<ll> dp(m + 1);
+    rep(j, 0, m + 1) dp[j] = j;
+    rep(i, 1, n + 1){
+        last = i;
+        rep(j, 1, m + 1){
+            curr = min(min(dp[j], last) + 1, dp[j - 1] + (str1[i - 1] != str2[j - 1]));
+            dp[j - 1] = last;
+            last = curr;
+        }
+        dp[m] = curr;
+    }
+    return dp[m];
+}
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+    string str1, str2;
+    cin >> str1;
+    cin >> str2;
+    cout << editDistance(str1, str2);
+}
